9b0a4cf305e94a297cad03aac668c5b16644cc46
Prakhar <prakhar@everything.net.in>
2022-03-09 14:19:11 +0530

    components added
    Signed-off-by: Prakhar <prakhar@everything.net.in>

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import Header from "../components/Header";
+import Head from "next/head";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <CssBaseline />
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,79 @@
-import Head from "next/head";
-import CssBaseline from "@mui/material/CssBaseline";
 import Submit from "../components/Submit";
-import Header from "../components/Header";
 import { Grid } from "@mui/material";
+import PropertyCard from "../components/PropertyCard";
+import {
+  LineChart,
+  ResponsiveContainer,
+  Legend,
+  Tooltip,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+} from "recharts";
+
+const pdata = [
+  {
+    name: "MongoDb",
+    student: 11,
+    fees: 120,
+  },
+  {
+    name: "Javascript",
+    student: 15,
+    fees: 12,
+  },
+  {
+    name: "PHP",
+    student: 5,
+    fees: 10,
+  },
+  {
+    name: "Java",
+    student: 10,
+    fees: 5,
+  },
+  {
+    name: "C#",
+    student: 9,
+    fees: 4,
+  },
+  {
+    name: "C++",
+    student: 10,
+    fees: 8,
+  },
+];
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <CssBaseline />
-      <Header />
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: "100vh" }}
-      >
+      <Grid container spacing={2}>
         <Grid item xs={3}>
-          <Submit />
+          <PropertyCard />
+        </Grid>
+        <Grid item xs={3}>
+          <PropertyCard />
+        </Grid>
+        <Grid item xs={3}>
+          <PropertyCard />
+        </Grid>
+        <Grid item xs={3}>
+          <PropertyCard />
+        </Grid>
+        <Grid item xs={12}>
+          <h1 className="text-heading">Line Chart Using Rechart</h1>
+          <ResponsiveContainer width="100%" aspect={3}>
+            <LineChart data={pdata} margin={{ right: 300 }}>
+              <CartesianGrid />
+              <XAxis dataKey="name" interval={"preserveStartEnd"} />
+              <YAxis></YAxis>
+              <Legend />
+              <Tooltip />
+              <Line dataKey="student" stroke="black" activeDot={{ r: 8 }} />
+              <Line dataKey="fees" stroke="red" activeDot={{ r: 8 }} />
+            </LineChart>
+          </ResponsiveContainer>
         </Grid>
       </Grid>
     </div>
